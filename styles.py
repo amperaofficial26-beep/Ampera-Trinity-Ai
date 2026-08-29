762d473d2d16248d17a4a592dbe1cece734d52f0
Ampera_Official <amperaofficial26@gmail.com>
2026-08-30 00:54:54 +0700

    Update styles.py

--- styles.py
+++ styles.py
@@ -389,7 +389,7 @@ section[data-testid="stSidebar"] .element-container { margin: 0 !important; }
 }
 [class*="st-key-msg_actions_"] [data-testid="stHorizontalBlock"] > [data-testid="stColumn"]:nth-child(2),
 [class*="st-key-msg_actions_"] [data-testid="stHorizontalBlock"] > [data-testid="stColumn"]:nth-child(3) {
-    margin-top: -6px !important;
+    margin-top: -10px !important;
 }
 [class*="st-key-msg_actions_"] [data-testid="stHorizontalBlock"] {
     display: flex !important;
