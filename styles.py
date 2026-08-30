8887e1df88f114d69964dfa40dda1971dda64564
Ampera_Official <amperaofficial26@gmail.com>
2026-08-31 01:06:23 +0700

    Increase footer font size to 4.0rem

--- styles.py
+++ styles.py
@@ -1259,7 +1259,7 @@ div.stDownloadButton > button:hover {
 
 /* ---------- footer ---------- */
 .trinity-foot {
-    text-align: center; color: #7E7387; font-size: 0.40rem;
+    text-align: center; color: #7E7387; font-size: 4.0rem;
     margin-top: 34px; font-family: 'Inter', sans-serif;
 }
 /* versi saat chat berjalan: lebih kecil lagi dari versi halaman awal */
