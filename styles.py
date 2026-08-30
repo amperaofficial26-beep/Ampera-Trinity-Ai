d5aee0eb30fe0a841f935461a39d14ffcccdfaf4
Ampera_Official <amperaofficial26@gmail.com>
2026-08-30 23:12:26 +0700

    Reduce footer font size in styles.py

--- styles.py
+++ styles.py
@@ -1286,7 +1286,7 @@ div.stDownloadButton > button:hover {
 
 /* ---------- footer ---------- */
 .trinity-foot {
-    text-align: center; color: #7E7387; font-size: 0.30rem;
+    text-align: center; color: #7E7387; font-size: 0.10rem;
     margin-top: 34px; font-family: 'Inter', sans-serif;
 }
 /* versi saat chat berjalan: lebih kecil lagi dari versi halaman awal */
