8e223834af379cf7848acd46da15b5e0b9ff1343
Ampera_Official <amperaofficial26@gmail.com>
2026-08-31 10:01:08 +0700

    Adjust footer font sizes for better readability

--- styles.py
+++ styles.py
@@ -1259,12 +1259,12 @@ div.stDownloadButton > button:hover {
 
 /* ---------- footer ---------- */
 .trinity-foot {
-    text-align: center; color: #7E7387; font-size: 0.2rem;
+    text-align: center; color: #7E7387; font-size: 9px;
     margin-top: 34px; font-family: 'Inter', sans-serif;
 }
 /* versi saat chat berjalan: lebih kecil lagi dari versi halaman awal */
 .trinity-foot.in-chat {
-    font-size: 0.6rem;
+    font-size: 7px;
     color: #827788;
     margin-top: 22px;
 }
