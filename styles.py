f6b56b56de213122b7d6434c8340e0d3747fb6ed
Ampera_Official <amperaofficial26@gmail.com>
2026-08-31 09:58:21 +0700

    Update styles.py

--- styles.py
+++ styles.py
@@ -53,7 +53,7 @@ section[data-testid="stSidebar"] [data-testid="stSidebarHeader"] {
 /* tombol tutup sidebar — ikon Material, melayang di pojok */
 section[data-testid="stSidebar"] [data-testid="stSidebarHeader"] > * {
     position: absolute;
-    top: 25px; right: -40px;
+    top: 25px; right: -38px;
     z-index: 10;
 }
 [data-testid="stSidebarCollapseButton"] button {
