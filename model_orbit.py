edf94e65773809837566df474b4f519afc74a537
Ampera_Official <amperaofficial26@gmail.com>
2026-09-05 23:04:57 +0700

    Update model_orbit.py

--- model_orbit.py
+++ model_orbit.py
@@ -32,7 +32,7 @@ WARNA_LATAR = "#3C3489"      # latar popover (ungu ruang angkasa)
 WARNA_GARIS = "#3A2B4A"      # garis tepi popover & node
 WARNA_AKSEN = "#E8B04B"      # emas: elektron, node aktif, glow
 WARNA_TEKS = "#FFFFFF"       # teks utama
-WARNA_REDUP = "#E8B04B"      # teks deskripsi
+WARNA_REDUP = "#FFFFFF"      # teks deskripsi
 TINGGI_ORBIT_PX = 150        # tinggi area animasi orbit
 
 
