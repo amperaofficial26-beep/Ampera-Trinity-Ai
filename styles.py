1f655ede12e0753adac6921b3c7248823240de10
Ampera_Official <amperaofficial26@gmail.com>
2026-09-23 06:59:08 +0700

    Update styles.py

--- styles.py
+++ styles.py
@@ -3578,7 +3578,7 @@ section[data-testid="stSidebar"] [data-testid="stBaseButton-primary"] [data-test
     --topbar-left-open: 150px;      /* posisi kiri saat sidebar terbuka */
     --topbar-left-closed: 76px;     /* posisi kiri saat sidebar tertutup */
     --topbar-right-safe: 200px;     /* jarak aman dari panel kanan */
-    --topbar-max-width: 650px;      /* panjang maksimal kolom atas */
+    --topbar-max-width: 450px;      /* panjang maksimal kolom atas */
 }
 
 /* TOPBAR saat sidebar terbuka */
