366d38cb4e009cd5caa0bab50feb065f060fb54b
Ampera_Official <amperaofficial26@gmail.com>
2026-09-23 12:17:27 +0700

    Update styles.py

--- styles.py
+++ styles.py
@@ -4131,7 +4131,7 @@ section[data-testid="stSidebar"] [data-testid="stBaseButton-primary"] [data-test
        ========================= */
 
     /* Geser seluruh bubble percakapan kiri/kanan */
-    --conversation-x: -270px;
+    --conversation-x: -200px;
 
     /* Geser seluruh bubble percakapan atas/bawah */
     --conversation-y: 0px;
