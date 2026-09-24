e8d3ead88cdfe225c70ec6cbc310dac9ab32b5f4
Ampera_Official <amperaofficial26@gmail.com>
2026-09-25 03:59:42 +0700

    Update page_multi_agent.py

--- page_multi_agent.py
+++ page_multi_agent.py
@@ -23,7 +23,7 @@ WIB = ZoneInfo("Asia/Jakarta")
 # JUDUL + DESKRIPSI
 # ------------------------------------------------------------
 MULTI_HERO_X = -220
-MULTI_HERO_Y = -50
+MULTI_HERO_Y = -25
 
 # ------------------------------------------------------------
 # CHAT SURFACE
